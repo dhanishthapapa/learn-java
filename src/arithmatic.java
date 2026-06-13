a83f71325ec8fd2b64b51a200a975a4f75183739
dhanishthapapa <sridhanishthapapa@gmail.com>
2026-06-13 23:14:01 +0530

    added

--- src/arithmatic.java
+++ src/arithmatic.java
@@ -11,7 +11,8 @@ public class arithmatic {
         System.out.println(d);
         System.out.println(c);
         for(int i=0;i<5;i++){
-            System.out.println(i);
+            System.out.println(i+=i);
         }
+        System.out.println("hello world");
     }
 }
