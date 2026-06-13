3029e26161af7b5969d08b247883790dc6009ace
dhanishthapapa <sridhanishthapapa@gmail.com>
2026-06-13 23:32:49 +0530

    string

--- src/string.java
+++ src/string.java
@@ -1,3 +1,14 @@
 public class string {
-    
+    public static void main(String [] args){
+        String name="John";
+        String name2="Doe";
+        String fullName=name+" "+name2;
+        System.out.println(fullName);
+        String message="Hello, World!";
+        System.out.println(message.length());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+        System.out.println(message.charAt(7));
+        System.out.println(message.substring(0,5));
+    }
 }
