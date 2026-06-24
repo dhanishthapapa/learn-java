95dcbf1f5da82f3b2ed7e73dbde8b7b27c88614e
dhanishthapapa <sridhanishthapapa@gmail.com>
2026-06-24 09:07:44 +0530

    java bank project

--- src/variables.java
+++ src/variables.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Scanner;
 public class variables {
     public static void main(String []args){
        //primitive data types
@@ -10,13 +11,19 @@ public class variables {
        char grade='A';
        System.out.println("grade "+grade);
        float ok=0.987f;
-        System.out.printf("float value %.2f ",ok); 
+        System.out.printf("float value %.1f ",ok); 
         int size=94;
         System.out.println("\nsize "+size);
         Date today=new Date();
         System.out.print("time: " +today.getHours()+":"+today.getMinutes()+":"+today.getSeconds());
         System.out.println("\ntoday is "+today);
+        Date today2=new Date();
+        System.out.println("today2 is "+today2);
         int bankBalance=1000;
+        System.out.println("bankBalance "+bankBalance);
+        Scanner bank=new Scanner(System.in);
+        int deposit=bank.nextInt();
+        bankBalance+=deposit;
         System.out.println("bankBalance "+bankBalance);
     }
 }
